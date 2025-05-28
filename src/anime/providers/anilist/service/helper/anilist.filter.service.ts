ddddf8a43f1a0853fe1bf87a247c3b056578550f
daffineq <168743697+daffineq@users.noreply.github.com>
2025-05-28 22:12:28 +0300

    filter fixed

--- src/anime/providers/anilist/service/helper/anilist.filter.service.ts
+++ src/anime/providers/anilist/service/helper/anilist.filter.service.ts
@@ -348,7 +348,7 @@ export class AnilistFilterService {
         ]
       }
 
-      if (field === 'updated_at') return { zoro: { updatedAt: direction } }
+      if (field === 'updated_at') return { zoro: { updatedAt: direction === 'desc' ? 'asc' : 'desc' } }
 
       if (parts.length > 1) {
         let nested: any = { [parts.pop()!]: direction }
