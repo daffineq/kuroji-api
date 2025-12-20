7fb505ac3778c11795a04b6cb41cf5227bf80eca
daffineq <168743697+daffineq@users.noreply.github.com>
2025-12-21 00:55:07 +0200

    state endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { HTTPError } from 'ky';
 import Elysia, { NotFoundError } from 'elysia';
 import { cors } from '@elysiajs/cors';
 import swagger from '@elysiajs/swagger';
+import { prisma } from './lib/prisma';
 
 const app = new Elysia()
   .use(
@@ -69,6 +70,10 @@ const app = new Elysia()
             description: 'Main REST endpoints'
           },
           {
+            name: 'System',
+            description: 'System endpoints'
+          },
+          {
             name: 'Proxy',
             description: 'Proxy endpoints'
           },
@@ -120,16 +125,70 @@ app.use(animeRoute());
 app.use(apiRoute());
 app.use(proxyRoute());
 
-app.get('/graphql', ({ request }) => yoga.handle(request));
-app.post('/graphql', ({ request }) => yoga.handle(request));
-
-app.get('/logs', ({ query }) => {
-  const page = Math.max(1, Number(query.page ?? 1));
-  const perPage = Math.min(100, Math.max(1, Number(query.per_page ?? 50)));
-
-  const logs = logger.getLogsPaginated(page, perPage);
-
-  return createSuccessResponse({ message: 'Logs got', data: logs });
+app.get('/graphql', ({ request }) => yoga.handle(request), {
+  tags: ['GraphQL']
 });
+
+app.post('/graphql', ({ request }) => yoga.handle(request), {
+  tags: ['GraphQL']
+});
+
+app.get(
+  '/logs',
+  ({ query }) => {
+    const page = Math.max(1, Number(query.page ?? 1));
+    const perPage = Math.min(100, Math.max(1, Number(query.per_page ?? 50)));
+
+    const logs = logger.getLogsPaginated(page, perPage);
+
+    return createSuccessResponse({ message: 'Logs got', data: logs });
+  },
+  {
+    tags: ['System']
+  }
+);
+
+app.get(
+  '/state',
+  async () => {
+    const uptime = (): string => {
+      const uptime = process.uptime();
+      const hours = Math.floor(uptime / 3600);
+      const minutes = Math.floor((uptime % 3600) / 60);
+      const seconds = Math.floor(uptime % 60);
+
+      return `${hours}h ${minutes}m ${seconds}s`;
+    };
+
+    const size =
+      (await prisma.$queryRaw`SELECT pg_size_pretty(pg_database_size(current_database())) AS size;`) as {
+        size: string;
+      }[];
+
+    const memory = process.memoryUsage();
+
+    const dbHealthy = await prisma.$queryRaw`SELECT 1`;
+
+    return createSuccessResponse({
+      message: 'State of API',
+      data: {
+        uptime: uptime(),
+        db: {
+          size: size[0]?.size,
+          healthy: dbHealthy ? true : false
+        },
+        memory: {
+          rss: `${(memory.rss / 1024 / 1024).toFixed(2)} MB`,
+          heapTotal: `${(memory.heapTotal / 1024 / 1024).toFixed(2)} MB`,
+          heapUsed: `${(memory.heapUsed / 1024 / 1024).toFixed(2)} MB`,
+          external: `${(memory.external / 1024 / 1024).toFixed(2)} MB`
+        }
+      }
+    });
+  },
+  {
+    tags: ['System']
+  }
+);
 
 export default app;
