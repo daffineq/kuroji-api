6819867ebfd90f0ac40bab66a7976cfe0fb3e89b
daffineq <168743697+daffineq@users.noreply.github.com>
2026-01-09 00:17:11 +0200

    bumped the timeout

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,12 @@ import { Config } from 'src/config/config';
 import { PrismaClient } from 'src/prisma/generated/client';
 
 const adapter = new PrismaPg({ connectionString: Config.database_url });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter,
+  transactionOptions: {
+    timeout: 15000
+  }
+});
 
 export { prisma };
 export * from 'src/prisma/generated/client';
