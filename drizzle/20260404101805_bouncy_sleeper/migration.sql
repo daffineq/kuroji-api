f812b25b55e4a3aeb86093cb177d1ff1cc973dc9
daffineq <168743697+daffineq@users.noreply.github.com>
2026-04-04 13:18:20 +0300

    migration

---
+++ drizzle/20260404101805_bouncy_sleeper/migration.sql
@@ -0,0 +1,6 @@
+CREATE INDEX "idx_anime_genre_name" ON "anime_genre" ("name");--> statement-breakpoint
+CREATE INDEX "idx_anime_studio_name" ON "anime_studio" ("name");--> statement-breakpoint
+CREATE INDEX "idx_anime_tag_name" ON "anime_tag" ("name");--> statement-breakpoint
+CREATE INDEX "idx_anime_title_romaji" ON "anime_title" ("romaji");--> statement-breakpoint
+CREATE INDEX "idx_anime_title_english" ON "anime_title" ("english");--> statement-breakpoint
+CREATE INDEX "idx_anime_title_native" ON "anime_title" ("native");
