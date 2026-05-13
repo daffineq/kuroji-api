68cadee9f122b5ac56b85fe79957510dc2872f7c
daffineq <168743697+daffineq@users.noreply.github.com>
2026-05-13 22:23:10 +0300

    fixed indexer

--- src/db/schema/indexer.ts
+++ src/db/schema/indexer.ts
@@ -3,5 +3,6 @@ import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 export const indexerState = pgTable('indexer_state', {
   id: varchar('id', { length: 255 }).primaryKey(),
   last_page: integer('last_page').notNull(),
+  last_pl: integer('last_pl'),
   updated_at: timestamp('updated_at').notNull().defaultNow()
 });
